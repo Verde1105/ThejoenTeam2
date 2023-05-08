221ea710149866abf97cb4d5e4f3994a2d6aa110
Verde1105 <lunaflo@naver.com>
2023-05-09 00:25:11 +0900

    프로젝트

---
+++ ThejoenTeam2/src/main/java/com/thejoen/team2/ThejoenTeam2Application.java
@@ -0,0 +1,13 @@
+package com.thejoen.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThejoenTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThejoenTeam2Application.class, args);
+	}
+
+}
---
+++ ThejoenTeam2/src/test/java/com/thejoen/team2/ThejoenTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejoen.team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThejoenTeam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
